ec0bd8c328da0af2551b35d7d9972245f80a4f66
hechenkuan <cnima88@126.com>
2020-08-03 10:03:09 +0800

    导入模块

---
+++ drawings/sqare.py
@@ -0,0 +1,11 @@
+import turtle # 导入一个模块(turtle)
+
+turtle.forward(100) # 调用 forward 方法前进100步
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+
+turtle.mainloop()
