02ad6c6150a4ed13dd4c2504129cd458fc3f3161
hechenkuan <cnima88@126.com>
2020-08-03 10:45:58 +0800

    三角形

---
+++ drawings/turtle_demo.py
@@ -0,0 +1,19 @@
+import turtle
+turtle.pencolor('green')
+turtle.goto(100,0)
+turtle.goto(50,7500**0.5)
+turtle.goto(0,0)
+
+turtle.pencolor('red')
+turtle.goto(100,0)
+turtle.goto(50,75)
+turtle.goto(0,0)
+
+turtle.pencolor('blue')
+turtle.goto(100,0)
+turtle.goto(50,85)
+turtle.goto(0,0)
+
+
+
+turtle.mainloop()
