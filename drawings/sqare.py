b8b761d1e28068a309dbe6c90b79a11f7f49e372
hechenkuan <cnima88@126.com>
2020-08-05 10:32:02 +0800

    变量和运算符

--- drawings/sqare.py
+++ drawings/sqare.py
@@ -1,6 +1,6 @@
 import turtle # 导入一个模块(turtle)
  # 调用 forward 方法前进100步
-for i in range(4):
+for i in range():
     turtle.forward(100)
     turtle.left(90)
 turtle.mainloop()
---
+++ variables.py
@@ -0,0 +1,24 @@
+"""  变量
+"""
+
+# 整数
+a = 1
+print(a)
+print(type(a))
+
+# 浮点数
+a = 1/3
+print(a)
+print(type(a))
+a = a*3-1
+print(a)
+
+# 布尔值
+a = True
+print(a)
+print(type(a))
+
+# 字符串
+a ='乐德学堂'
+print(a)
+print(type(a))
